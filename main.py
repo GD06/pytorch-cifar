66253d003fa7e3742e7b6f6994698fcb1ccc088c
GD06 <xinfeng@ucsb.edu>
2019-04-09 13:53:17 -0700

    Update main.py to support specifying model name in the command line Other changes including exposing the flexibility of specifying the directory of data and the directory of checkpoint

--- main.py
+++ main.py
@@ -16,13 +16,65 @@ import argparse
 from models import *
 from utils import progress_bar
 
+def _build_network(model_name):
+    net = None 
+    if model_name == 'VGG16':
+        net = VGG('VGG16')
+    elif model_name == 'ResNet18':
+        net = ResNet18()
+    elif model_name == 'ResNet50':
+        net = ResNet50() 
+    elif model_name == 'ResNet101':
+        net = ResNet101()
+    elif model_name == 'MobileNetV2':
+        net = MobileNetV2()
+    elif model_name == 'GoogLeNet':
+        net = GoogLeNet()
+
+    assert net != None
+    return net 
+
+# net = VGG('VGG19')
+# net = ResNet18()
+# net = PreActResNet18()
+# net = GoogLeNet()
+# net = DenseNet121()
+# net = ResNeXt29_2x64d()
+# net = MobileNet()
+# net = MobileNetV2()
+# net = DPN92()
+# net = ShuffleNetG2()
+# net = SENet18()
+# net = ShuffleNetV2(1)
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('model', help='the name of model to be trained')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true', 
+        help='resume from checkpoint')
+parser.add_argument('--checkpoint_dir', default=None, help='the directory of'
+        ' checkpoint models')
+parser.add_argument('--data_dir', default=None, help='the directory storing data')
+parser.add_argument('--num_epoches', default=10, type=int, help='the number of'
+        ' epoches for training')
+parser.add_argument('--batchsize', default=128, type=int, help='the batch size of'
+        ' each training iteration')
+
+args = parser.parse_args()
+
 work_dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(work_dir)
 
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
-parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
-args = parser.parse_args()
+if args.checkpoint_dir is None:
+    checkpoint_dir = os.path.join(work_dir, "checkpoint", args.model)
+else:
+    checkpoint_dir = args.checkpoint_dir 
+
+if args.data_dir is None:
+    data_dir = os.path.join(work_dir, "data")
+else:
+    data_dir = args.data_dir 
+
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
@@ -42,28 +94,22 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, 
+        download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batchsize, 
+        shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, 
+        download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=args.batchsize, 
+        shuffle=False, num_workers=2)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 
+        'horse', 'ship', 'truck')
 
 # Model
 print('==> Building model..')
-# net = VGG('VGG19')
-# net = ResNet18()
-# net = PreActResNet18()
-# net = GoogLeNet()
-# net = DenseNet121()
-# net = ResNeXt29_2x64d()
-# net = MobileNet()
-# net = MobileNetV2()
-# net = DPN92()
-# net = ShuffleNetG2()
-# net = SENet18()
-net = ShuffleNetV2(1)
+net = _build_network(args.model)
 net = net.to(device)
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
@@ -73,7 +119,7 @@ if args.resume:
     # Load checkpoint.
     print('==> Resuming from checkpoint..')
     assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.t7')
+    checkpoint = torch.load(os.path.join(checkpoint_dir, 'ckpt.t7'))
     net.load_state_dict(checkpoint['net'])
     best_acc = checkpoint['acc']
     start_epoch = checkpoint['epoch']
@@ -133,12 +179,12 @@ def test(epoch):
             'acc': acc,
             'epoch': epoch,
         }
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/ckpt.t7')
+        if not os.path.isdir(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+        torch.save(state, os.path.join(checkpoint_dir, 'ckpt.t7'))
         best_acc = acc
 
-
-for epoch in range(start_epoch, start_epoch+200):
+for epoch in range(start_epoch, start_epoch + args.num_epoches):
     train(epoch)
     test(epoch)
+
